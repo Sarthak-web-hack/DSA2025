a2b5f32d2298290b3c4d1e5bd49b5ca0d09ca31c
sarthak jagatap <pushkrajjagatap.com>
2025-08-21 06:00:02 +0530

    Solid  rhombus

---
+++ solidrhombus.java
@@ -0,0 +1,24 @@
+class solidrhombus
+{
+    public void rhombus()
+    {
+        int  n=5;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=(n-i);j++)
+            {
+                System.out.print(" ");
+
+            }
+            for(int j=1;j<=n;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        solidrhombus s1=new solidrhombus();
+        s1.rhombus();
+    }
+}
