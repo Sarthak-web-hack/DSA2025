3b73c6563b6bb00d7d581af52bdf70d033582456
sarthak jagatap <pushkrajjagatap.com>
2025-08-25 19:42:05 +0530

    collecting the all possible pair of array

---
+++ pairinarray.java
@@ -0,0 +1,20 @@
+class pairinarray
+{
+    public void pair(int num[])
+    {
+        for(int i=0;i<num.length;i++)
+        {
+            int curr=num[i];
+            for(int j=i+1;j<num.length;j++)
+            {
+                System.out.print("(" +curr + ","+num[j] +") ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pairinarray p1=new pairinarray();
+        int num[]={2,4,6,8,10};
+        p1.pair(num);
+    }
+}
